6dee2c4ea86a901c3171c9ebcf7fcac590ec0d11
김민경 <kmg6077>
2021-08-17 11:33:52 +0900

    공지사항 글쓰기 추가

--- app/src/main/java/com/example/smartvest/InfoActivity.java
+++ app/src/main/java/com/example/smartvest/InfoActivity.java
@@ -1,6 +1,9 @@
 package com.example.smartvest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,7 @@ public class InfoActivity extends AppCompatActivity {
 
     ListView lv;
     List<InfoVO> data;
+    Button btn_write;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,15 @@ public class InfoActivity extends AppCompatActivity {
 
         lv = findViewById(R.id.lv);
         data = new ArrayList<InfoVO>();
+        btn_write = findViewById(R.id.btn_write);
 
+        btn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), InfoWriteActivity.class);
+                startActivity(intent);
+            }
+        });
         for (int i = 1; i < 10; i++){
             data.add(new InfoVO("공지사항"+i,
                     "2021-08-0"+i));
---
+++ app/src/main/java/com/example/smartvest/InfoWriteActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartvest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_write);
+    }
+}
