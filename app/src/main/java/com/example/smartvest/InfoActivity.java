816e1097eb6cbcc1397901c592f0182e0d5b07fb
김민경 <kmg6077>
2021-08-17 15:25:48 +0900

    성민이 오류 확인용 업데이트

--- app/src/main/java/com/example/smartvest/InfoActivity.java
+++ app/src/main/java/com/example/smartvest/InfoActivity.java
@@ -2,11 +2,20 @@ package com.example.smartvest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +31,9 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference productRef = db.collection("InfoWrite");
+
         lv = findViewById(R.id.lv);
         data = new ArrayList<InfoVO>();
         btn_write = findViewById(R.id.btn_write);
@@ -33,16 +45,36 @@ public class InfoActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        for (int i = 1; i < 10; i++){
-            data.add(new InfoVO("공지사항"+i,
-                    "2021-08-0"+i));
-        }
 
+        //Set up the ArrayList
+        data = new ArrayList<InfoVO>();
 
+        //set the Adapter
         InfoAdapter adapter = new InfoAdapter(getApplicationContext(),
                 R.layout.infolist, data);
 
         lv.setAdapter(adapter);
+
+        db.collection("InfoWrite").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                List<InfoVO> lv = new ArrayList<>();
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document : task.getResult()) {
+                        InfoVO info = document.toObject(InfoVO.class);
+                        data.add(info);
+                    }
+                    ListView lvlv = (ListView)findViewById(R.id.lv);
+                    InfoAdapter adapter = new InfoAdapter(getApplicationContext(),
+                            R.layout.infolist, data);
+
+                    lvlv.setAdapter(adapter);
+                } else {
+                    Log.d("InfoActivity", "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/example/smartvest/InfoWriteActivity.java
+++ app/src/main/java/com/example/smartvest/InfoWriteActivity.java
@@ -1,14 +1,56 @@
 package com.example.smartvest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class InfoWriteActivity extends AppCompatActivity {
+
+    EditText et_infoTitle, et_infoWrite;
+    Button btn_infoOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_write);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        et_infoTitle = findViewById(R.id.et_infoTitle);
+        et_infoWrite = findViewById(R.id.et_infoWrite);
+        btn_infoOk = findViewById(R.id.btn_infoOk);
+
+
+        String title = et_infoTitle.getText().toString();
+        String write = et_infoTitle.getText().toString();
+//        String time = new Date(timeStamp).toDateString();
+
+        Map<String, Object> info = new HashMap<>();
+//        info.put("날짜", date);
+        info.put("내용", write);
+        info.put("제목", title);
+
+        btn_infoOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(InfoWriteActivity.this, "등록 성공", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
